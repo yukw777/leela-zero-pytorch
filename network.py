f6c5f41ea3c3e0b76060539b9a058aad2eb8236b
Peter Yu <2057325+yukw777@users.noreply.github.com>
2019-11-06 01:53:45 -0500

    New ConvBlock module.
    a convolutional layer is always followed by a batch norm layer, which is optionally followed by a ReLU.
    All the convolutional layers have been replaced with ConvBlock

--- network.py
+++ network.py
@@ -2,35 +2,49 @@ import torch
 import torch.nn as nn
 
 
-def conv3x3(in_channels, out_channels):
-    """3x3 convolution with padding"""
-    return nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+class ConvBlock(nn.Module):
+    """
+    A convolutional block with a convolution layer, batchnorm and an optional relu
+    """
 
+    def __init__(self, in_channels, out_channels, kernel_size, relu=True):
+        super(ConvBlock, self).__init__()
+        # we only support the kernel sizes of 1 and 3
+        assert kernel_size in (1, 3)
 
-def conv1x1(in_channels, out_channels):
-    """1x1 convolution"""
-    return nn.Conv2d(in_channels, out_channels, kernel_size=1)
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=1 if kernel_size == 3 else 0)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True) if relu else None
+
+        # initializations
+        nn.init.kaiming_normal_(self.conv.weight, mode='fan_out', nonlinearity='relu')
+        nn.init.constant_(self.bn.weight, 1)
+        nn.init.constant_(self.bn.bias, 0)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        return self.relu(x) if self.relu is not None else x
 
 
 class ResBlock(nn.Module):
 
     def __init__(self, in_channels, out_channels):
         super(ResBlock, self).__init__()
-        self.conv1 = conv3x3(in_channels, out_channels)
-        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv1 = ConvBlock(in_channels, out_channels, 3)
+        self.conv2 = ConvBlock(out_channels, out_channels, 3, relu=False)
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(out_channels, out_channels)
-        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        nn.init.constant_(self.conv2.bn.weight, 0)
 
     def forward(self, x):
         identity = x
 
         out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
         out = self.conv2(out)
-        out = self.bn2(out)
 
         out += identity
         out = self.relu(out)
@@ -42,38 +56,22 @@ class Network(nn.Module):
 
     def __init__(self, board_size, in_channels, residual_channels, residual_layers):
         super(Network, self).__init__()
-        self.conv_input = conv3x3(in_channels, residual_channels)
-        self.bn_input = nn.BatchNorm2d(residual_channels)
-        self.relu_input = nn.ReLU(inplace=True)
+        self.conv_input = ConvBlock(in_channels, residual_channels, 3)
         self.residual_tower = nn.Sequential(
             *[ResBlock(residual_channels, residual_channels) for _ in range(residual_layers)])
-        self.policy_conv = conv1x1(residual_channels, 2)
+        self.policy_conv = ConvBlock(residual_channels, 2, 1)
         self.policy_fc = nn.Linear(2 * board_size * board_size, board_size * board_size + 1)
-        self.value_conv = conv1x1(residual_channels, 1)
-        self.value_fc = nn.Sequential(*[
+        self.value_conv = ConvBlock(residual_channels, 1, 1)
+        self.value_fc = nn.Sequential(
             nn.Linear(board_size * board_size, 256),
             nn.ReLU(inplace=True),
             nn.Linear(256, 1),
             nn.Tanh(),
-        ])
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-            elif isinstance(m, ResBlock):
-                # Zero-initialize the last BN in each residual branch,
-                # so that the residual branch starts with zeros, and each residual block behaves like an identity.
-                # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
-                nn.init.constant_(m.bn2.weight, 0)
+        )
 
     def forward(self, x):
         # first conv layer
         x = self.conv_input(x)
-        x = self.bn_input(x)
-        x = self.relu_input(x)
 
         # residual tower
         x = self.residual_tower(x)
