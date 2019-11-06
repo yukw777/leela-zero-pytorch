4d704928b918e1cc07ba92ae4a97c1beb46533ec
Peter Yu <2057325+yukw777@users.noreply.github.com>
2019-11-06 02:04:33 -0500

    no bias for convolutional layers

--- network.py
+++ network.py
@@ -12,7 +12,13 @@ class ConvBlock(nn.Module):
         # we only support the kernel sizes of 1 and 3
         assert kernel_size in (1, 3)
 
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=1 if kernel_size == 3 else 0)
+        self.conv = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            padding=1 if kernel_size == 3 else 0,
+            bias=False
+        )
         self.bn = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True) if relu else None
 
