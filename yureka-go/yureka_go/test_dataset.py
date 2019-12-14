c6d6f084314df30b408718b4492a52e7a626b48a
Peter Yu <2057325+yukw777@users.noreply.github.com>
2019-12-14 17:29:15 -0500

    stone_plane test done

--- yureka-go/yureka_go/test_dataset.py
+++ yureka-go/yureka_go/test_dataset.py
@@ -1,15 +1,50 @@
 import pytest
 import torch
 
-from yureka_go.dataset import move_plane
-
-
-def test_stone_plane():
-    pass
+from yureka_go.dataset import move_plane, stone_plane
 
 
 @pytest.mark.parametrize(
-    "turn_bytes,planes",
+    'hex_bytes,plane',
+    (
+        (
+            # nothing
+            hex(int(b'0000000000000000000' * 19, 2))[2:],
+            torch.zeros(19, 19),
+        ),
+        (
+            # upper left corner
+            hex(int(b'1000000000000000000' + b'0000000000000000000' * 18, 2))[2:],
+            torch.tensor([1] + [0] * 360).float().view(19, 19),
+        ),
+        (
+            # upper right corner
+            hex(int(b'0000000000000000001' + b'0000000000000000000' * 18, 2))[2:],
+            torch.tensor([0] * 18 + [1] + [0] * 342).float().view(19, 19),
+        ),
+        (
+            # bottom left corner
+            hex(int(b'0000000000000000000' * 18 + b'1000000000000000000', 2))[2:],
+            torch.tensor([0] * 342 + [1] + [0] * 18).float().view(19, 19),
+        ),
+        (
+            # bottom right corner
+            hex(int(b'0000000000000000000' * 18 + b'0000000000000000001', 2))[2:],
+            torch.tensor([0] * 360 + [1]).float().view(19, 19),
+        ),
+        (
+            # middle
+            hex(int(b'0000000000000000000' * 9 + b'0000000001000000000' + b'0000000000000000000' * 9, 2))[2:],
+            torch.tensor([0] * 180 + [1] + [0] * 180).float().view(19, 19),
+        ),
+    )
+)
+def test_stone_plane(hex_bytes: bytes, plane: torch.Tensor):
+    assert stone_plane(hex_bytes).equal(plane)
+
+
+@pytest.mark.parametrize(
+    'turn_bytes,planes',
     (
         (b'0', torch.stack([torch.ones(19, 19), torch.zeros(19, 19)])),
         (b'1', torch.stack([torch.zeros(19, 19), torch.ones(19, 19)])),
