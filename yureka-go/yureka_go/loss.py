b6f10de1340264c4593d09889ab60d27d1a6e000
Peter Yu <2057325+yukw777@users.noreply.github.com>
2019-12-17 10:46:21 -0500

    loss accepts two arguments, each tuple

--- yureka-go/yureka_go/loss.py
+++ yureka-go/yureka_go/loss.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn.functional as F
 
+from typing import Tuple
+
 from flambe.metric import Metric
 
 
@@ -10,8 +12,10 @@ class MSECrossEntropyLoss(Metric):
         super().__init__()
         self.alpha = alpha
 
-    def compute(self, pred_move: torch.Tensor, pred_val: torch.Tensor,
-                target_move: torch.Tensor, target_val: torch.Tensor) -> torch.Tensor:
+    def compute(self, pred: Tuple[torch.Tensor, torch.Tensor],
+                target: Tuple[torch.Tensor, torch.Tensor]) -> torch.Tensor:
+        pred_move, pred_val = pred
+        target_move, target_val = target
         cross_entropy_loss = F.cross_entropy(pred_move, target_move)
         mse_loss = F.mse_loss(pred_val, target_val)
         return self.alpha * mse_loss + cross_entropy_loss
