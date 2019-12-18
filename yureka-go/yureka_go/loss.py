c33073494c3a1710858b8775a187906e71db1607
Peter Yu <2057325+yukw777@users.noreply.github.com>
2019-12-18 16:30:32 -0500

    squeeze the predictions when calculating the loss

--- yureka-go/yureka_go/loss.py
+++ yureka-go/yureka_go/loss.py
@@ -15,6 +15,8 @@ class MSECrossEntropyLoss(Metric):
     def compute(self, pred: Tuple[torch.Tensor, torch.Tensor],
                 target: Tuple[torch.Tensor, torch.Tensor]) -> torch.Tensor:
         pred_move, pred_val = pred
+        pred_move = pred_move.squeeze()
+        pred_val = pred_val.squeeze()
         target_move, target_val = target
         cross_entropy_loss = F.cross_entropy(pred_move, target_move)
         mse_loss = F.mse_loss(pred_val, target_val)
