7004fb5a0061985c2cc1243ea63fb53c9ac828a5
Peter Yu <2057325+yukw777@users.noreply.github.com>
2020-02-27 18:32:55 -0500

    a new training script using hydra and pytorch lightning

---
+++ leela_zero_pytorch/train.py
@@ -0,0 +1,42 @@
+import hydra
+
+from pytorch_lightning import Trainer
+from torch.utils.data import DataLoader
+
+from leela_zero_pytorch.network import Network
+from leela_zero_pytorch.dataset import Dataset
+
+
+@hydra.main(config_path='../conf/config.yaml')
+def train(cfg):
+    print(cfg.pretty())
+    network = Network(
+        cfg.network.board_size,
+        cfg.network.in_channels,
+        cfg.network.residual_channels,
+        cfg.network.residual_layers,
+    )
+    trainer = Trainer(max_epochs=cfg.train.max_epochs)
+    trainer.fit(
+        network,
+        train_dataloader=DataLoader(
+            Dataset.from_data_dir(hydra.utils.to_absolute_path(cfg.dataset.train_dir)),
+            shuffle=True,
+            batch_size=cfg.train.batch_size,
+            num_workers=cfg.train.n_data_workers,
+        ),
+        val_dataloaders=DataLoader(
+            Dataset.from_data_dir(hydra.utils.to_absolute_path(cfg.dataset.val_dir)),
+            batch_size=cfg.train.batch_size,
+            num_workers=cfg.train.n_data_workers,
+        ),
+        test_dataloaders=DataLoader(
+            Dataset.from_data_dir(hydra.utils.to_absolute_path(cfg.dataset.test_dir)),
+            batch_size=cfg.train.batch_size,
+            num_workers=cfg.train.n_data_workers,
+        ),
+    )
+
+
+if __name__ == "__main__":
+    train()
