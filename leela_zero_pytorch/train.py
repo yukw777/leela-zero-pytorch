62aa8dbd0423eb91f6e8f13592b7c88bd325fc4e
Peter Yu <2057325+yukw777@users.noreply.github.com>
2020-07-23 15:15:44 -0400

    Hydra 1.0.0rc2

--- leela_zero_pytorch/train.py
+++ leela_zero_pytorch/train.py
@@ -1,11 +1,9 @@
 import logging
-import argparse
+import hydra
 
-from pathlib import Path
-from omegaconf import OmegaConf
+from omegaconf import DictConfig
 from pytorch_lightning import Trainer
 from torch.utils.data import DataLoader
-from hydra.experimental import compose, initialize
 from hydra.utils import instantiate
 
 from leela_zero_pytorch.network import NetworkLightningModule
@@ -15,31 +13,29 @@ from leela_zero_pytorch.dataset import Dataset
 logger = logging.getLogger(__name__)
 
 
-def main() -> Trainer:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("overrides", nargs="*", default=[])
-    args = parser.parse_args()
-
-    parsed = Path("conf/config.yaml")
-    initialize(config_dir=str(parsed.parent), strict=False)
-    cfg = compose(parsed.name, overrides=args.overrides)
+@hydra.main(config_path="conf", config_name="config")
+def main(cfg: DictConfig) -> Trainer:
     logger.info(f"Training with the following config:\n{cfg.pretty()}")
 
     # we want to pass in dictionaries as OmegaConf doesn't play nicely with
     # loggers and doesn't allow non-native types
-    module = NetworkLightningModule(OmegaConf.to_container(cfg.train, resolve=True))
-    trainer_logger = instantiate(cfg.logger) if cfg.logger is not None else True
+    module = NetworkLightningModule(cfg.train)
+    trainer_logger = instantiate(cfg.logger) if "logger" in cfg else True
     trainer = Trainer(**cfg.pl_trainer, logger=trainer_logger)
     trainer.fit(
         module,
         train_dataloader=DataLoader(
-            Dataset.from_data_dir(cfg.dataset.train.dir_path, transform=True),
+            Dataset.from_data_dir(
+                hydra.utils.to_absolute_path(cfg.dataset.train.dir_path), transform=True
+            ),
             shuffle=True,
             batch_size=cfg.dataset.train.batch_size,
             num_workers=cfg.dataset.train.num_workers,
         ),
         val_dataloaders=DataLoader(
-            Dataset.from_data_dir(cfg.dataset.val.dir_path),
+            Dataset.from_data_dir(
+                hydra.utils.to_absolute_path(cfg.dataset.val.dir_path)
+            ),
             batch_size=cfg.dataset.val.batch_size,
             num_workers=cfg.dataset.val.num_workers,
         ),
@@ -47,8 +43,10 @@ def main() -> Trainer:
     if cfg.train.run_test:
         trainer.test(
             test_dataloaders=DataLoader(
-                Dataset.from_data_dir(cfg.dataset.test.dir_path),
-                batch_size=cfg.datset.train.batch_size,
+                Dataset.from_data_dir(
+                    hydra.utils.to_absolute_path(cfg.dataset.test.dir_path)
+                ),
+                batch_size=cfg.dataset.train.batch_size,
                 num_workers=cfg.dataset.test.num_workers,
             )
         )
--- tests/test_train.py
+++ tests/test_train.py
@@ -1,6 +1,8 @@
 import pytest
 import sys
 
+from hydra.experimental import initialize, compose
+from omegaconf import open_dict
 from pytorch_lightning.loggers import TensorBoardLogger, WandbLogger
 
 from leela_zero_pytorch.train import main as train_main
@@ -8,31 +10,34 @@ from leela_zero_pytorch.weights import main as weights_main
 
 
 @pytest.mark.parametrize(
-    "logger", [[], ["logger=wandb", "logger.params.offline=true"]],
+    "logger", [[], ["logger=null"]],
 )
-def test_train(monkeypatch, tmp_path, capsys, clear_hydra, logger):
-    monkeypatch.setattr(
-        sys,
-        "argv",
-        [
-            "lzp-train",
-            "network=small",
-            "dataset.train.dir_path=tests/test-data",
-            "dataset.train.batch_size=2",
-            "dataset.val.dir_path=tests/test-data",
-            "dataset.val.batch_size=2",
-            "dataset.test.dir_path=tests/test-data",
-            "dataset.test.batch_size=2",
-            f"pl_trainer.default_root_dir={tmp_path}",
-            "pl_trainer.fast_dev_run=true",
-        ]
-        + logger
-        + ([f"logger.params.save_dir={tmp_path}"] if len(logger) > 0 else []),
-    )
-    with capsys.disabled():
-        # both pytest and wandb capture stdout, and they cause
-        # a deadlock, so don't capture when running the trainer
-        trainer = train_main()
+def test_train(monkeypatch, tmp_path, capsys, logger):
+    with initialize(config_path="../leela_zero_pytorch/conf"):
+        cfg = compose(
+            config_name="config",
+            overrides=[
+                "+network=small",
+                "dataset.train.dir_path=tests/test-data",
+                "dataset.train.batch_size=2",
+                "dataset.val.dir_path=tests/test-data",
+                "dataset.val.batch_size=2",
+                "dataset.test.dir_path=tests/test-data",
+                "dataset.test.batch_size=2",
+                f"+pl_trainer.default_root_dir={tmp_path}",
+                "+pl_trainer.fast_dev_run=true",
+            ]
+            + logger,
+        )
+        if len(logger) == 0:
+            # default wandb logger
+            with open_dict(cfg):
+                cfg.logger.params.offline = True
+                cfg.logger.params.save_dir = str(tmp_path)
+        with capsys.disabled():
+            # both pytest and wandb capture stdout, and they cause
+            # a deadlock, so don't capture when running the trainer
+            trainer = train_main(cfg)
 
     checkpoint_path = "checkpoints/epoch=0.ckpt"
     if isinstance(trainer.logger, TensorBoardLogger):
